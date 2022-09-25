218a29fac3efba45dd52ee880c4ca94ed0f28510
Vilena Amanova <79453600+vilenaamanova@users.noreply.github.com>
2022-09-25 14:13:30 +0300

    changed

--- prov.c
+++ prov.c
@@ -1,3 +1,5 @@
+#include "minishell.h"
+
 	// t_envpmod *prov;
 	// prov = shell->envp;
 	// while (prov)
@@ -14,3 +16,28 @@
 	// 	printf("%s\n", (char *)prov->content);
 	// 	prov = prov->next;
 	// }
+
+void find_builtin(t_shell *shell)
+{
+	t_parser	*parser;
+
+	parser = (t_parser *)shell->commands->content;
+
+	printf("CMD LIST CONTENT IN FIND BUILTIN:\n");
+	printf("%s\n", (char *)parser->cmd_list->content);
+
+	if (ft_strncmp("cd", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content)) == 0)
+		ft_cd(shell);
+	if (ft_strncmp("pwd", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content)) == 0)
+		ft_pwd();
+	if (ft_strncmp("env", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content) + 1) == 0)
+		env_pr(shell, shell->envp);
+	if (ft_strncmp("export", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content) + 1) == 0)
+		ft_export(shell);
+	if (ft_strncmp("unset", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content) + 1) == 0)
+		delete_env(shell, shell->parser);
+	if (ft_strncmp("exit", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content) + 1) == 0)
+		ft_exit(shell);
+	if (ft_strncmp("echo", (char *)parser->cmd_list->content, ft_strlen((char *)parser->cmd_list->content) + 1) == 0)
+		ft_echo(shell);
+}
